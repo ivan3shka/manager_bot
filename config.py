4069044223f7d14e679fc33cffcd5beaebaae0b2
ivan <iabaryshev228@hmail.com>
2025-03-10 14:15:46 -0300

    Исправление создания бд

--- config.py
+++ config.py
@@ -11,6 +11,14 @@ DEFAULT_COMMANDS = (
     ('start', 'Начать с начала'),
 )
 
-DB_PATH = os.getenv("DB_PATH", "/app/data/database.db")
+DB_PATH = os.getenv("DB_PATH", "data/database.db")
 
+# Получаем папку, где должна быть БД
+DB_DIR = os.path.dirname(DB_PATH)
 
+# Если путь относительный, создаём папку в текущем каталоге
+if not os.path.isabs(DB_PATH):
+    DB_DIR = os.path.join(os.getcwd(), DB_DIR)
+
+# Создаём папку, если её нет
+os.makedirs(DB_DIR, exist_ok=True)
